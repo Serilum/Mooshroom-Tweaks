bdec8e5dcbc2faeb5e2529da96f2dbcf527c0517
Rick South <rick@natamus.com>
2023-12-12 09:40:29 +0100

    Mooshroom Tweaks source code for 1.20.2.

---
+++ Common/src/main/java/com/natamus/mooshroomtweaks/ModCommon.java
@@ -0,0 +1,15 @@
+package com.natamus.mooshroomtweaks;
+
+import com.natamus.mooshroomtweaks.config.ConfigHandler;
+
+public class ModCommon {
+
+	public static void init() {
+		ConfigHandler.initConfig();
+		load();
+	}
+
+	private static void load() {
+		
+	}
+}
---
+++ NeoForge/src/main/java/com/natamus/mooshroomtweaks/neoforge/events/NeoForgeMooshroomEvent.java
@@ -0,0 +1,14 @@
+package com.natamus.mooshroomtweaks.neoforge.events;
+
+import com.natamus.mooshroomtweaks.events.MooshroomEvent;
+import net.neoforged.neoforge.event.entity.EntityJoinLevelEvent;
+import net.neoforged.bus.api.SubscribeEvent;
+import net.neoforged.fml.common.Mod.EventBusSubscriber;
+
+@EventBusSubscriber
+public class NeoForgeMooshroomEvent {
+	@SubscribeEvent
+	public static void onEntityJoin(EntityJoinLevelEvent e) {
+		MooshroomEvent.onEntityJoin(e.getLevel(), e.getEntity());
+	}
+}
