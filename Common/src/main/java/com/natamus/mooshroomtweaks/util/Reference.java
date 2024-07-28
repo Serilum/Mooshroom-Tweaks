382bfa325de9a2447afe27a21fa9d5ea2be481ef
Rick South <rick@natamus.com>
2024-07-28 07:25:20 +0200

    (v3.5) Added Bundle support. The mod can now be enabled/disabled via bundle configs. [+1]

--- Common/src/main/java/com/natamus/mooshroomtweaks/util/Reference.java
+++ Common/src/main/java/com/natamus/mooshroomtweaks/util/Reference.java
@@ -3,6 +3,6 @@ package com.natamus.mooshroomtweaks.util;
 public class Reference {
 	public static final String MOD_ID = "mooshroomtweaks";
 	public static final String NAME = "Mooshroom Tweaks";
-	public static final String VERSION = "3.4";
+	public static final String VERSION = "3.5";
 	public static final String ACCEPTED_VERSIONS = "[1.20.6]";
 }
--- NeoForge/src/main/java/com/natamus/mooshroomtweaks/ModNeoForge.java
+++ NeoForge/src/main/java/com/natamus/mooshroomtweaks/ModNeoForge.java
@@ -1,6 +1,7 @@
 package com.natamus.mooshroomtweaks;
 
 import com.natamus.collective.check.RegisterMod;
+import com.natamus.collective.check.ShouldLoadCheck;
 import com.natamus.mooshroomtweaks.neoforge.config.IntegrateNeoForgeConfig;
 import com.natamus.mooshroomtweaks.neoforge.events.NeoForgeMooshroomEvent;
 import com.natamus.mooshroomtweaks.util.Reference;
@@ -15,6 +16,10 @@ import net.neoforged.fml.javafmlmod.FMLJavaModLoadingContext;
 public class ModNeoForge {
 	
 	public ModNeoForge(IEventBus modEventBus) {
+		if (!ShouldLoadCheck.shouldLoad(Reference.MOD_ID)) {
+			return;
+		}
+
 		modEventBus.addListener(this::loadComplete);
 
 		setGlobalConstants();
