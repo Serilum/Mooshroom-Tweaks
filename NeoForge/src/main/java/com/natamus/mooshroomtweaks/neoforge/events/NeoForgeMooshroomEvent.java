65fde4869ee818eb5ceef748cde05ed15f04a9e3
Rick South <rick@natamus.com>
2025-02-12 23:25:36 +0100

    (v3.6) (Forge & Neo) Removed redundant event register annotations. [+1]

--- NeoForge/src/main/java/com/natamus/mooshroomtweaks/neoforge/events/NeoForgeMooshroomEvent.java
+++ NeoForge/src/main/java/com/natamus/mooshroomtweaks/neoforge/events/NeoForgeMooshroomEvent.java
@@ -3,9 +3,7 @@ package com.natamus.mooshroomtweaks.neoforge.events;
 import com.natamus.mooshroomtweaks.events.MooshroomEvent;
 import net.neoforged.neoforge.event.entity.EntityJoinLevelEvent;
 import net.neoforged.bus.api.SubscribeEvent;
-import net.neoforged.fml.common.EventBusSubscriber;
 
-@EventBusSubscriber
 public class NeoForgeMooshroomEvent {
 	@SubscribeEvent
 	public static void onEntityJoin(EntityJoinLevelEvent e) {
