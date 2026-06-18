34f43acba6646b01c69c3e22eea18beb12412b9e
Rick South <rick@natamus.com>
2026-06-18 23:40:25 +0200

    Updated Mooshroom Tweaks to Minecraft version 26.2.0.

--- Common/src/main/java/com/natamus/mooshroomtweaks/util/Reference.java
+++ Common/src/main/java/com/natamus/mooshroomtweaks/util/Reference.java
@@ -4,5 +4,5 @@ public class Reference {
 	public static final String MOD_ID = "mooshroomtweaks";
 	public static final String NAME = "Mooshroom Tweaks";
 	public static final String VERSION = "3.6";
-	public static final String ACCEPTED_VERSIONS = "[26.1.2]";
+	public static final String ACCEPTED_VERSIONS = "[26.2.0]";
 }
